11cb343f38c65c67932c0b0a4dfc84fc9f19c464
its-jhaanjali-29 <jhaanjali2903@gmail.com>
2025-09-04 10:15:50 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+    // var swiper = new Swiper(".mySwiper", {
+    //   navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    //   },
+    // });
+  
+
+  var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  navigation: {
+    nextEl: "#next",
+    prevEl: "#prev",
+  },
+});
